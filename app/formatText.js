8750c4a537ab80b9f6806111a4413893118855d0
Benjamin <benjamin@Benjamins-MacBook-Air.local>
2017-11-27 23:48:14 +0000

    Moved functions that format the text to 'formatText' module. The output now delegates that functionality so things are more visible and easier to understand

--- app/formatText.js
+++ app/formatText.js
@@ -2,8 +2,6 @@ function FormatText(data) {
   this._data = data;
 }
 
-<<<<<<< HEAD
-=======
 FormatText.prototype.toDowncase = function() {
   return this._data = this._data.toLowerCase();
 };
@@ -24,5 +22,4 @@ FormatText.prototype.countWords = function() {
   return this._data = this._data.reduce((dict, v) => {dict[v] = v in dict ? dict[v] + 1 : 1; return dict}, {});
 }
 
->>>>>>> e3ca7e417ac633d7ac480fc603adc131e2ae186c
 module.exports = FormatText;
