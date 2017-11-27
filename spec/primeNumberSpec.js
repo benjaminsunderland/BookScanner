7229a74e5ba418b8ce4be193acb8721f5ea2b0af
Benjamin <benjamin@Benjamins-MacBook-Air.local>
2017-11-27 09:34:28 +0000

    Added final tests that check if a number passed into the function returns 'false' as a prime number

--- spec/primeNumberSpec.js
+++ spec/primeNumberSpec.js
@@ -23,5 +23,34 @@ describe("I will return true", function() {
   it("when given 13 as prime number", function() {
     expect(primeNumber.isPrime(13)).toEqual(true)
     });
+  it("when given 67 as prime number", function() {
+    expect(primeNumber.isPrime(67)).toEqual(true)
+    });
+  it("when given 71 as prime number", function() {
+    expect(primeNumber.isPrime(71)).toEqual(true)
+    });
+  it("when given 89 as prime number", function() {
+    expect(primeNumber.isPrime(89)).toEqual(true)
+    });
   });
+describe("I will return false", function() {
+  it("when given 4 as prime number", function() {
+    expect(primeNumber.isPrime(4)).toEqual(false)
+    });
+  it("when given 12 as prime number", function() {
+    expect(primeNumber.isPrime(12)).toEqual(false)
+    });
+  it("when given 40 as prime number", function() {
+    expect(primeNumber.isPrime(40)).toEqual(false)
+    });
+  it("when given 70 as prime number", function() {
+    expect(primeNumber.isPrime(70)).toEqual(false)
+    });
+  it("when given 84 as prime number", function() {
+    expect(primeNumber.isPrime(84)).toEqual(false)
+    });
+  it("when given 10- as prime number", function() {
+    expect(primeNumber.isPrime(100)).toEqual(false)
+  });
+ });
 });
