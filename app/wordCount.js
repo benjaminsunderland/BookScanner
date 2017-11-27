3928e76561c5e8d6ebf8369b022f5b26a2e90fa8
Benjamin <benjamin@Benjamins-MacBook-Air.local>
2017-11-27 23:49:28 +0000

    'turntoPrime' function now delegates functionality to the primeNumber module

--- app/wordCount.js
+++ app/wordCount.js
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-var fs = require('fs')
-var PrimeNumber = require("../app/primeNumber.js");
-var FormatText = require("../app/formatText.js");
-
-function WordCount(filepath, primenumber = new PrimeNumber, formattext = new FormatText){
-  this._filepath = filepath || null
-  this._primenumber = primenumber
-=======
 var fs = require("fs");
 var PrimeNumber = require("../app/primeNumber.js");
 var FormatText = require("../app/formatText.js");
@@ -14,7 +5,6 @@ var FormatText = require("../app/formatText.js");
 function WordCount(filepath, primenumber = new PrimeNumber(), formattext = new FormatText()) {
   this._filepath = filepath || null;
   this._primenumber = primenumber;
->>>>>>> e3ca7e417ac633d7ac480fc603adc131e2ae186c
   this._formattext = formattext;
 }
 
@@ -31,29 +21,12 @@ WordCount.prototype.turnToPrime = function(words) {
   return this._primenumber.wordPrime(words)
 };
 
-PrimeNumber.prototype.primeNumber = function(words) {
-  self = this;
-  return Object.keys(words).map(function(key) {
-  return { [key]: words[key], prime: self.this_primenumber(words[key]) };
-  });
-}
-
 WordCount.prototype.printOutput = function() {
-<<<<<<< HEAD
-  x = this.countTerms((this.removeEmptyEntries(this.wordSplit(this.removeWords(this.toDowncase(this.readfile()))))))
-  return primeNumber(x)
-}
-=======
   this._formattext.toDowncase()
   this._formattext.deleteWords()
   this._formattext.splitWords()
   this._formattext.filterEntries()
   return this.turnToPrime(this._formattext.countWords())
 };
->>>>>>> e3ca7e417ac633d7ac480fc603adc131e2ae186c
 
 module.exports = WordCount;
-
-word = new WordCount('../CTM_Test/spec/TextFiles/fortestingwithtdd.txt')
-word.readfile()
-console.log(word.printOutput())
